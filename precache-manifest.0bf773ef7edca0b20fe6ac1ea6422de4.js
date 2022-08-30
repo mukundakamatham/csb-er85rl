a07e817acd1b97e1c0710810fcaf8de0b63e25df
Code Sandbox <hello@codesandbox.io>
2022-08-30 13:28:18 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.0bf773ef7edca0b20fe6ac1ea6422de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33aff8a0d6dbe9c8cb9ee99d3c963fb",
+    "url": "https://mukundakamatham.github.io/csb-er85rl/index.html"
+  },
+  {
+    "revision": "c18726c25222dd6fe5a9",
+    "url": "https://mukundakamatham.github.io/csb-er85rl/static/css/main.910aa548.chunk.css"
+  },
+  {
+    "revision": "04baa17a579a93d47d48",
+    "url": "https://mukundakamatham.github.io/csb-er85rl/static/js/2.23ff9a0c.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "https://mukundakamatham.github.io/csb-er85rl/static/js/2.23ff9a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18726c25222dd6fe5a9",
+    "url": "https://mukundakamatham.github.io/csb-er85rl/static/js/main.ca29159c.chunk.js"
+  },
+  {
+    "revision": "858105bd62cac5d94b19",
+    "url": "https://mukundakamatham.github.io/csb-er85rl/static/js/runtime-main.cd300ac0.js"
+  }
+]);
